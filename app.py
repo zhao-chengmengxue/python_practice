296d291df7b9e8907f6e484eddf0ea7a6716341c
zhao-chengmengxue <zhaochengmengxue@gmail.com>
2022-11-07 09:10:42 +0800

    22.11.17
    today's practice

---
+++ app.py
@@ -0,0 +1,55 @@
+# print("zcmx")
+# print("o-----")
+# print("  ||||")
+# print("*" * 10)
+
+# price = 10
+# ratting = 4.9
+# name = 'Mosh'
+# is_pubished = False
+# print(price)
+"""储存的值可以是数字（整数或浮点数）、字符串（字符串要加引号）、布尔值"""
+# name = 'jon'
+# age = 12
+# is_new = True
+
+# name = input('What is your full-name? ')
+# print('Hi ' + name)
+
+# name = input("what is your name?  ")
+# fav_color = input("What is your favorite color?  ")
+# print(name + " likes " + fav_color)
+
+# birth_year = input("birth year:  ")
+# print(type(birth_year))
+# age = 2021 - int(birth_year)
+# print(type(age))
+# print(age)
+"""input函数会返回一个字符串，不能直接参与数字运算，要转换一下"""
+# weight = input("your weight: ")
+# kilo = int(weight) * 0.45
+# print(kilo)
+
+"""灵活使用单引号，双引号和三引号！"""
+# course = 'python for beginners'
+# print(course[0])
+# print(course[-1])
+# print(course[0:3])
+# print(course[0:])
+# print(course[:3])
+# print(course[:])
+"""从0开始数，这个字符串的空格也占一个位置, -1是最后一个字符, 0:3包括0位置，但不包括3位置"""
+
+# name = "jennifer"
+# print(name[1:-1])
+# first = "John"
+# last = "Smith"
+# message = first + " [" +last +"] is a coder"
+# msg = f'{first} [{last}] is a coder'
+# print(message)
+# print(msg)
+"""运用formated string,要在开头先打一个f‘’，然后在句中需要赋值的地方用卷括号括起来"""
+
+course = "Python for beginners"
+print(len(course))
+print(course.upper())
