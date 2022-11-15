a128d121d7aa24e7f02a29c643480e96ff8bffc3
zhao-chengmengxue <zhaochengmengxue@gmail.com>
2022-11-15 21:55:28 +0800

    Create 2.py
    new practice 2022/11/15
    
    if

---
+++ 2.py
@@ -0,0 +1,58 @@
+# course = 'Python for beginners'
+# print(course.replace('P','J'))
+# print('Python' in course)
+# print('python' in course)
+# len()
+# course.upper()
+# course.lower()
+# print(course.title())
+# course.find()
+# course.replace()
+# '..' in course
+
+# print(10 + 3)
+# print(10 - 3)
+# print(10 * 3)
+# print(10 / 3)
+# """加减乘除"""
+# print(10 // 3)
+# """整除"""
+# print(10 % 3)
+# """取余"""
+# print(10 ** 3)
+# """power"""
+
+# x = 10
+# x += 3
+# print(x)
+
+# x = -2.9
+# print(round(x))
+# print(abs(x))
+
+# import math
+# print(math.ceil(-2.9))
+# print(math.floor(-2.9))
+# print(math.ceil(2.9))
+# print(math.floor(2.9))
+
+# is_hot = True
+# is_cold = False
+#
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink more water")
+# elif is_cold:
+#     print("It's a cold day")
+#     print("Wear warm cloth")
+# else:
+#     print("It's a lovely day")
+# print("Have a nice day!")
+
+good_credit = True
+price = 100
+if good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+print(f"The down payment is ${down_payment}")
